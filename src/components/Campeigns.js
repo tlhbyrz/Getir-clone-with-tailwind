01d5612ccc588408b991c462b516b5e5e9a35ffc
tlhbyrz <talhaboyraz13@gmail.com>
2021-08-30 01:43:07 +0300

    category and campeign added

--- src/components/Campeigns.js
+++ src/components/Campeigns.js
@@ -1,9 +1,57 @@
 import React from 'react'
+import Slider from "react-slick";
+import campaignData from "data/banners.json"
+import { AiOutlineLeft, AiOutlineRight } from "react-icons/ai"
+
+
+function SampleNextArrow(props) {
+    const { onClick } = props
+    return (
+      <button onClick={onClick} className="absolute top-1/2 -right-6 -translate-y-1/2">
+          <AiOutlineRight size={22} />
+      </button>
+    );
+  }
+  
+  function SamplePrevArrow(props) {
+    const { onClick } = props
+    return (
+      <button onClick={onClick} className="absolute top-1/2 -left-6 -translate-y-1/2">
+            <AiOutlineLeft size={22} />
+        </button>
+    );
+  }
 
 function Campeigns() {
+
+    const settings = {
+        dots: false,
+        infinite: true,
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        autoplay: true,
+        speed: 4000,
+        autoplaySpeed: 4000,
+        cssEase: "linear",
+        nextArrow: <SampleNextArrow />,
+        prevArrow: <SamplePrevArrow />
+    };
+
+
     return (
-        <div>
-            Campeigns
+        <div className="container mx-auto mt-10"> 
+            <h3 className="text-sm font-semibold mb-3">Kampanyalar</h3>
+            <Slider {...settings}>
+                {
+                    campaignData.map(item => (
+                        <div key={item.id}>
+                            <picture className="block px-2">
+                                <img className="w-full object-cover rounded-lg"  src={item.image} alt="" />
+                            </picture>
+                        </div>
+                    ))
+                }
+            </Slider>
         </div>
     )
 }
--- src/components/Categories.js
+++ src/components/Categories.js
@@ -1,9 +1,24 @@
 import React from 'react'
+import categoryData from "data/categories.json"
 
 function Categories() {
     return (
-        <div>
-            Categories
+        <div className="bg-white py-4">
+            <div className="container mx-auto">
+                <h3 className="text-sm font-semibold mb-3">Kategoriler</h3>
+                <div className="grid grid-cols-10 gap-y-3">
+                    {
+                        categoryData.map(item => (
+                            <a href="#" key={item.id} className="flex flex-col p-3 transition rounded-lg
+                                hover:bg-purple-100 items-center text-center group">
+                                <img src={item.image} alt={item.title} lazy={true} 
+                                className="rounded w-12 h-12" />
+                                <span className="text-sm font-semibold text-gray-500 group-hover:text-gray-800">{item.title}</span>
+                            </a>
+                        ))
+                    }
+                </div>
+            </div>
         </div>
     )
 }
